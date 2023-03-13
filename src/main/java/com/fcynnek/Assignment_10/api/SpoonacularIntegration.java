a0f501eea8565517e5245da4cd4d1729c778a196
fcynnek <kchengfung@gmail.com>
2023-03-13 19:09:29 -0400

    Initializing Objects

--- src/main/java/com/fcynnek/Assignment_10/api/SpoonacularIntegration.java
+++ src/main/java/com/fcynnek/Assignment_10/api/SpoonacularIntegration.java
@@ -2,4 +2,8 @@ package com.fcynnek.Assignment_10.api;
 
 public class SpoonacularIntegration {
 
+	/* 
+	* API Key from Spoonacular:
+	* f86c8e27d2b84f2fac40c3c0974a933f 
+	*/
 }
--- src/main/java/com/fcynnek/Assignment_10/web/MealController.java
+++ src/main/java/com/fcynnek/Assignment_10/web/MealController.java
@@ -2,6 +2,8 @@ package com.fcynnek.Assignment_10.web;
 
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.fcynnek.Assignment_10.dto.ResponseEntity;
+
 public class MealController {
 
 	@GetMapping("mealplanner/week")
