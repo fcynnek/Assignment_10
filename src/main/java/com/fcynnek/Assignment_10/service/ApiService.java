1c662d0bf6a1304b2ae91b31e6ffb3fd7c9981ce
fcynnek <kchengfung@gmail.com>
2023-04-10 17:57:24 -0400

    day bug fixing in progress

--- src/main/java/com/fcynnek/Assignment_10/service/ApiService.java
+++ src/main/java/com/fcynnek/Assignment_10/service/ApiService.java
@@ -12,6 +12,7 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fcynnek.Assignment_10.api.SpoonacularIntegration;
 import com.fcynnek.Assignment_10.dto.DayResponse;
+import com.fcynnek.Assignment_10.dto.Week;
 import com.fcynnek.Assignment_10.dto.WeekResponse;
 import com.fcynnek.Assignment_10.repo.Meals;
 import com.fcynnek.Assignment_10.repo.Nutrients;
@@ -42,30 +43,8 @@ public class ApiService {
 	public DayResponse getDayMeals(String numCalories, String diet, String exclusions) {
 		DayResponse dayResponse = spoonacularAPI.getDayMeals(rt, numCalories, diet, exclusions);
 		
-		JsonObject jsonObject = gson.fromJson(gson.toJson(dayResponse), JsonObject.class);
-		parsingMealsAndNutrientsFromJson(dayResponse, jsonObject);
-		
-		return dayResponse;
-	}
-
-
-	
-	public WeekResponse getWeekMeals(String numCalories, String diet, String exclusions) {
-		WeekResponse weekResponse = spoonacularAPI.getWeekMeals(rt, numCalories, diet, exclusions);
-		
-		JsonObject jsonObject = gson.fromJson(gson.toJson(weekResponse), JsonObject.class);
-		DayResponse dayResponse = new DayResponse();
-		parsingMealsAndNutrientsFromJson(dayResponse, jsonObject);
-		
-//		weekResponse.setWeek(weekArray);
-		
-		return weekResponse;
-		
-		
-	}
-	
-	public void parsingMealsAndNutrientsFromJson(DayResponse dayResponse, JsonObject jsonObject) {
-		JsonArray mealsArray = jsonObject.getAsJsonArray("meals");
+		JsonObject dayObject = gson.fromJson(gson.toJson(dayResponse), JsonObject.class);
+		JsonArray mealsArray = dayObject.getAsJsonArray("meals");
 		
 		List<Meals> parsedMeals = new ArrayList<>();
 		
@@ -89,7 +68,9 @@ public class ApiService {
 			
 			parsedMeals.add(parsedMeal);
 		}
-		
+		// fix this to parse the nutrients and set them
+		// then extract into its own method
+		//then call this 7 times for the week
 		Nutrients nutrients = dayResponse.getNutrients();
 		
 		Double calories = nutrients.getCalories();
@@ -105,5 +86,64 @@ public class ApiService {
 		
 		dayResponse.setMeals(parsedMeals);
 		dayResponse.setNutrients(parsedNutrients);
+		
+		return dayResponse;
 	}
+
+
+	
+	public WeekResponse getWeekMeals(String numCalories, String diet, String exclusions) {
+		WeekResponse weekResponse = spoonacularAPI.getWeekMeals(rt, numCalories, diet, exclusions);
+		
+		JsonObject jsonObject = gson.fromJson(gson.toJson(weekResponse), JsonObject.class);
+		JsonObject weeksObj = jsonObject.getAsJsonObject("week");
+//		JsonArray mealsArray = jsonObject.getAsJsonArray("week");
+		
+		
+		List<Meals> parsedMeals = new ArrayList<>();
+		
+		JsonArray mondayMealsArray = weeksObj.getAsJsonObject("monday").getAsJsonArray("meals");
+		
+		for (JsonElement mealElement : mealsArray) {
+			JsonObject mealsObject = mealElement.getAsJsonObject();
+			
+			Long id = mealsObject.get("id").getAsLong();
+			String imageType = mealsObject.get("imageType").getAsString();
+			String title = mealsObject.get("title").getAsString();
+			Integer readyInMinutes = mealsObject.get("readyInMinutes").getAsInt();
+			Integer servings = mealsObject.get("servings").getAsInt();
+			String sourceUrl = mealsObject.get("sourceUrl").getAsString();
+			
+			Meals parsedMeal = new Meals();
+			parsedMeal.setId(id);
+			parsedMeal.setImageType(imageType);
+			parsedMeal.setTitle(title);
+			parsedMeal.setReadyInMinutes(readyInMinutes);
+			parsedMeal.setServings(servings);
+			parsedMeal.setSourceUrl(sourceUrl);
+			
+			parsedMeals.add(parsedMeal);
+		}
+		DayResponse dayResponse = new DayResponse();
+		Nutrients nutrients = dayResponse.getNutrients();
+		
+		Double calories = nutrients.getCalories();
+		Double protein = nutrients.getProtein();
+		Double fat = nutrients.getFat();
+		Double carbohydrates = nutrients.getCarbohydrates();
+		
+		Nutrients parsedNutrients = new Nutrients();
+		parsedNutrients.setCalories(calories);
+		parsedNutrients.setProtein(protein);
+		parsedNutrients.setFat(fat);
+		parsedNutrients.setCarbohydrates(carbohydrates);
+		
+		dayResponse.setMeals(parsedMeals);
+		dayResponse.setNutrients(parsedNutrients);
+		
+		return weekResponse;
+		
+		
+	}
+	
 }
